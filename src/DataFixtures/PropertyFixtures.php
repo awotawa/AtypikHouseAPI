2c0ae31e91c153373ac5ea06fa9e4482e6578189
Alexandre Wotawa <awotawa@gmail.com>
2022-05-01 15:38:28 +0200

    restored the PropertyFixtures file

---
+++ src/DataFixtures/PropertyFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use DateTime;
+use DateTimeInterface;
+use App\Entity\Property;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
+
+class PropertyFixtures extends Fixture implements DependentFixtureInterface
+{
+
+  public function getDependencies()
+  {
+    return [
+      CategoryFixtures::class,
+    ];
+  }
+
+  public function load(ObjectManager $manager)
+  {
+
+    $faker = Faker\Factory::create("fr_FR");
+
+    for ($i=0; $i < 20; $i++) {
+
+      $property = new Property();
+      $property->setCategoryId($this->getReference("CATEGORY".mt_rand(0, 19)));
+      $property->setNewField($faker->randomElement(["Surface du logement ", "Nombre de pièces", "Nombre de chambres", "Hauteur Sol", "Eau", "Electricité", "Distance du parking"]));
+      $property->setDefaultValue($faker->randomElement(["20m² ", "2", "1", "2m", "Eau courante", "oui", "5km"]));
+      $property->setUpdatedAt($faker->dateTime());
+      $manager->persist($property);
+      $this->addReference("PROPERTY".$i, $property);
+    }
+
+    $manager->flush();
+  }
+}
